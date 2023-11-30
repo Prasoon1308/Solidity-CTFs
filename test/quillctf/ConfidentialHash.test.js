a311d760bf58865b1fb06a4b1063b15abf26e87b
Prasoon1308 <prasoon1308@gmail.com>
2023-11-30 22:57:25 +0530

    QuillCTF: ConfidentialHash updated

--- test/quillctf/ConfidentialHash.test.js
+++ test/quillctf/ConfidentialHash.test.js
@@ -1,12 +1,14 @@
-const {expect} = require("chai");
-const {ethers} = require("hardhat");
-const { loadFixture} = require("@nomicfoundation/hardhat-toolbox/network-helpers");
+const { expect } = require("chai");
+const { ethers } = require("hardhat");
+const {
+  loadFixture,
+} = require("@nomicfoundation/hardhat-toolbox/network-helpers");
 
-describe("QuillCTF 2: ConfidentialHash", ()=>{
+describe("QuillCTF 2: ConfidentialHash", () => {
   let contract;
   let owner;
   let attacker;
-  let contractAddress;  
+  let contractAddress;
 
   let hash;
 
@@ -23,20 +25,28 @@ describe("QuillCTF 2: ConfidentialHash", ()=>{
     return { owner, attacker, contract };
   }
 
-  it("should find the private variables", async()=>{
-    const {owner, attacker, contract} = await loadFixture(runEveryTime);
+  it("should find the private variables", async () => {
+    const { owner, attacker, contract } = await loadFixture(runEveryTime);
 
-    const aliceHash = await ethers.provider.getStorage(contractAddress, ethers.toQuantity(4));
-    const bobHash = await ethers.provider.getStorage(contractAddress, ethers.toQuantity(9));
+    const aliceHash = await ethers.provider.getStorage(
+      contractAddress,
+      ethers.toQuantity(4)
+    );
+    const bobHash = await ethers.provider.getStorage(
+      contractAddress,
+      ethers.toQuantity(9)
+    );
     // console.log(aliceHash, bobHash);
 
-    hash = ethers.solidityPackedKeccak256(["bytes32", "bytes32"], [aliceHash, bobHash]);
+    hash = ethers.solidityPackedKeccak256(
+      ["bytes32", "bytes32"],
+      [aliceHash, bobHash]
+    );
     // console.log(await contract.checkthehash(hash));
-
   });
 
-  after(async()=>{
+  after(async () => {
     // console.log(await contract.checkthehash(hash));
     expect(await contract.checkthehash(hash)).to.be.true;
   });
-})
+});
